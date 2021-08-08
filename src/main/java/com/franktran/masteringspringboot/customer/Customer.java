fbcfcefa6a9b8fd95ac98ae8a2ad668a75c9bc3a
frank <franktranvantu@gmail.com>
2021-08-08 21:12:12 +0700

    mastering-spring-boot-18 JsonIgnore and JSON Property

--- src/main/java/com/franktran/masteringspringboot/customer/Customer.java
+++ src/main/java/com/franktran/masteringspringboot/customer/Customer.java
@@ -1,15 +1,21 @@
 package com.franktran.masteringspringboot.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
 
   private final Long id;
   private final String name;
+  private final String password;
 
-  public Customer(Long id, String name) {
+  public Customer(Long id, String name, String password) {
     this.id = id;
     this.name = name;
+    this.password = password;
   }
 
+  @JsonProperty("customerId")
   public Long getId() {
     return id;
   }
@@ -18,11 +24,17 @@ public class Customer {
     return name;
   }
 
+  @JsonIgnore
+  public String getPassword() {
+    return password;
+  }
+
   @Override
   public String toString() {
     return "Customer{" +
         "id=" + id +
         ", name='" + name + '\'' +
+        ", password='" + password + '\'' +
         '}';
   }
 
