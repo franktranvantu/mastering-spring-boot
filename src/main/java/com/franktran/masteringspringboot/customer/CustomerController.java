4d23630cb1a97809d6df4f784ed567ca1ad8e118
frank <franktranvantu@gmail.com>
2021-08-08 18:54:20 +0700

    mastering-spring-boot-7 Dependency Injection

--- src/main/java/com/franktran/masteringspringboot/customer/CustomerController.java
+++ src/main/java/com/franktran/masteringspringboot/customer/CustomerController.java
@@ -1,15 +1,22 @@
 package com.franktran.masteringspringboot.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+  private final CustomerService customerService;
+
+  @Autowired
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
+
   @GetMapping
   public Customer getCustomer() {
-    Customer frank = new Customer(1L, "Frank");
-    return frank;
+    return customerService.getCustomer();
   }
 
 }
--- src/main/java/com/franktran/masteringspringboot/customer/CustomerService.java
+++ src/main/java/com/franktran/masteringspringboot/customer/CustomerService.java
@@ -1,5 +1,13 @@
 package com.franktran.masteringspringboot.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+
+  public Customer getCustomer() {
+    Customer frank = new Customer(1L, "Frank");
+    return frank;
+  }
 
 }
