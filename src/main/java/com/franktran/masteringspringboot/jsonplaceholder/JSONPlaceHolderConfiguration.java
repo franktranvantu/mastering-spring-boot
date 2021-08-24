03e47e0ae94195deafa5815861e675562afced1c
Frank <franktranvantu@gmail.com>
2021-08-24 16:32:03 +0700

    mastering-spring-boot-61 Test Calling API with OpenFeign

---
+++ src/main/java/com/franktran/masteringspringboot/jsonplaceholder/JSONPlaceHolderConfiguration.java
@@ -0,0 +1,17 @@
+package com.franktran.masteringspringboot.jsonplaceholder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JSONPlaceHolderConfiguration {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(JSONPlaceHolderClient jsonPlaceHolderClient) {
+        return args -> {
+            System.out.println(jsonPlaceHolderClient.getPosts().size());
+            System.out.println(jsonPlaceHolderClient.getPost(1));
+        };
+    }
+}
