47a5d5eea53a9a3aee6a0c36a71a86325b571585
Frank <franktranvantu@gmail.com>
2021-08-24 16:44:13 +0700

    mastering-spring-boot-63 Logging

--- src/main/java/com/franktran/masteringspringboot/customer/CustomerService.java
+++ src/main/java/com/franktran/masteringspringboot/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.franktran.masteringspringboot.customer;
 
 import com.franktran.masteringspringboot.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,6 +11,8 @@ import java.util.List;
 @Service
 public class CustomerService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
   private final CustomerRepository customerRepository;
 
   public CustomerService(CustomerRepository customerRepository) {
@@ -16,12 +20,17 @@ public class CustomerService {
   }
 
   public List<Customer> getCustomers() {
+    LOGGER.info("getCustomers() was called.");
     return customerRepository.findAll();
   }
 
   public Customer getCustomerById(@PathVariable Long id) {
     return customerRepository
             .findById(id)
-            .orElseThrow(() -> new NotFoundException(String.format("Customer with id %s not found", id)));
+            .orElseThrow(() -> {
+              NotFoundException notFoundException = new NotFoundException(String.format("Customer with id %s not found", id));
+              LOGGER.error(notFoundException.getMessage());
+              return notFoundException;
+            });
   }
 }
